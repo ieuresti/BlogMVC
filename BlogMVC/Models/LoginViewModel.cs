f6d93075be0791cdc072d5e4238f2ba565025e7a
Ivan Uresti <talyfuantos@gmail.com>
2025-11-17 16:41:38 -0600

    Se implementa login y registro de usuarios

---
+++ BlogMVC/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogMVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un correo electronico válido")]
+        public required string Email { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+        [Display(Name = "Recuerdame")]
+        public bool Recuerdame { get; set; }
+    }
+}
