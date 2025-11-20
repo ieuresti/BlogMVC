9854e04debfaf80eefb3743b27adce676a7b064a
Ivan Uresti <talyfuantos@gmail.com>
2025-11-19 18:44:45 -0600

    Se implementa funcionalidad crear entradas e instalacion de quill

---
+++ BlogMVC/Controllers/EntradasController.cs
@@ -0,0 +1,64 @@
+﻿using BlogMVC.Datos;
+using BlogMVC.Entidades;
+using BlogMVC.Models;
+using BlogMVC.Servicios;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogMVC.Controllers
+{
+    public class EntradasController: Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+        private readonly IServicioUsuarios servicioUsuarios;
+        private readonly string contenedor = "entradas";
+
+        public EntradasController(
+            ApplicationDbContext context,
+            IAlmacenadorArchivos almacenadorArchivos,
+            IServicioUsuarios servicioUsuarios)
+        {
+            this.context = context;
+            this.almacenadorArchivos = almacenadorArchivos;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = $"{Constantes.RolAdmin},{Constantes.CRUDEntradas}")]
+        public async Task<IActionResult> Crear(EntradaCrearViewModel modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(modelo);
+            }
+
+            string? portadaUrl = null;
+            if (modelo.ImagenPortada is not null)
+            {
+                portadaUrl = await almacenadorArchivos.Almacenar(contenedor, modelo.ImagenPortada);
+            }
+
+            string usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var entrada = new Entrada
+            {
+                Titulo = modelo.Titulo,
+                Cuerpo = modelo.Cuerpo,
+                PortadaUrl = portadaUrl,
+                FechaPublicacion = DateTime.UtcNow,
+                UsuarioCreacionId = usuarioId
+            };
+
+            context.Add(entrada);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Detalle", new { id = entrada.Id });
+        }
+    }
+}
---
+++ BlogMVC/wwwroot/js/formulario-entradas.js
@@ -0,0 +1,60 @@
+﻿const quill = new Quill('#editor', {
+    modules: {
+        toolbar: [
+            [{ header: [1, 2, false] }],
+            ['bold', 'italic', 'underline'],
+            ['code-block'],
+        ],
+    },
+    placeholder: 'Coloque aqui la entrada...',
+    theme: 'snow', // or 'bubble'
+});
+
+function btnEnviarClick() {
+    let esValido = validarFormularioCompleto();
+    if (!esValido) {
+        return;
+    }
+    // Obtener el contenido de Quill y convertirlo a JSON
+    const delta = quill.getContents();
+    const deltaJSON = JSON.stringify(delta.ops);
+    // Asignar el contenido JSON al campo oculto
+    $("#Cuerpo").val(deltaJSON);
+    $("#formEntrada").trigger("submit");
+}
+
+function validarFormularioCompleto() {
+    let formEntradaEsValido = $("#formEntrada").valid();
+    let cuerpoEsValido = validarCuerpo();
+
+    return formEntradaEsValido && cuerpoEsValido;
+}
+
+function validarCuerpo() {
+    let mensajeDeError = null;
+    let esValido = true;
+    const contenido = quill.getSemanticHTML();
+
+    if (contenido === '<p></p>') {
+        mensajeDeError = "El cuerpo es requerido";
+        esValido = false;
+    }
+
+    $("#cuerpo-error").html(mensajeDeError);
+    return esValido;
+}
+
+quill.on('text-change', function (delta, oldDelta, source) {
+    validarCuerpo();
+})
+
+function mostrarPrevisualizacion(event) {
+    const input = event.target; // Obtener el input file
+    const imagenPreview = document.getElementById('PreviewImagen');
+    if (input.files && input.files[0]) {
+        // Se crea una URL para la img seleccionada
+        const urlImagen = URL.createObjectURL(input.files[0]);
+        imagenPreview.src = urlImagen; // Se asigna la URL al src de la img
+        imagenPreview.style.display = 'block'; // Se muestra la img
+    }
+}
